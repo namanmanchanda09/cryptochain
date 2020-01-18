49fbabb1b0e69bcccc4a1734177215d5df5b1e60
namanmanchanda09 <naman.mohan09@outlook.com>
2020-01-18 22:31:53 +0530

    adjuested the difficulty in the mine rate

--- block.js
+++ block.js
@@ -15,12 +15,13 @@ class Block{
     static mineBlock({lastBlock,data}){
         let hash,timestamp;
         const lastHash = lastBlock.hash;
-        const {difficulty} = lastBlock;
+        let {difficulty} = lastBlock;
         let nonce = 0;
 
         do{
             nonce++;
             timestamp=Date.now();
+            difficulty=Block.adjustDifficulty({originalBlock:lastBlock,timestamp})
             hash=cryptoHash(timestamp,lastHash,data,nonce,difficulty);
         }
         while(hash.substring(0,difficulty)!=='0'.repeat(difficulty));
@@ -32,7 +33,7 @@ class Block{
 
     static adjustDifficulty({originalBlock,timestamp}){
         const {difficulty} = originalBlock;
-
+        if(difficulty<1) return 1;
         if((timestamp - originalBlock.timestamp)>MINE_RATE){
             return difficulty-1;
         }
--- block.test.js
+++ block.test.js
@@ -62,6 +62,12 @@ describe('Block',()=>{
             expect(minedBlock.hash.substring(0,minedBlock.difficulty))
             .toEqual('0'.repeat(minedBlock.difficulty))
         })
+
+        it('adjusts the difficulty',()=>{
+            const possibleResults = [lastBlock.difficulty+1,lastBlock.difficulty-1];
+            expect(possibleResults.includes(minedBlock.difficulty)).toBe(true);
+
+        })
     });
 
     describe('adjustDifficulty()', () => {
@@ -78,10 +84,16 @@ describe('Block',()=>{
             .toEqual(block.difficulty-1);
 
         });
+        it('has a lower limit of 1',()=>{
+            block.difficulty=-1;
+            expect(Block.adjustDifficulty({originalBlock:block})).toEqual(1);
+        })
 
     })
     
 })
+
+ 
 
 
 
