4c7ea327eb40b54d1ecb2f8f18919e932a4be8b7
namanmanchanda09 <naman.mohan09@outlook.com>
2020-01-21 01:04:47 +0530

    created redis pubsub class

--- pubsub.js
+++ pubsub.js
@@ -1,11 +1,30 @@
 const redis = require('redis');
 
+const CHANNELS = {
+    TEST:'TEST'
+};
+
+
 class PubSub{
     constructor(){
         this.pusblisher = redis.createClient();
         this.subscriber = redis.createClient();
+        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscriber.on('message',(channel,message)=>this.handleMessage(channel,message))
+    }
+    handleMessage(channel,message){
+        console.log(`Message received. Channel ${channel}. Message:${message}.`);
     }
 }
+
+const testPubSub = new PubSub();
+setTimeout(()=>testPubSub.pusblisher.publish(CHANNELS.TEST,'foo'),1000);
+
+
+
+
+
+
 
 
 
