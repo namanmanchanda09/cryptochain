4fb0a4daebb3e318f65f87373770d7d4fe2a3cf6
namanmanchanda09 <naman.mohan09@outlook.com>
2020-01-23 17:45:00 +0530

    broadcasting the chain

--- pubsub.js
+++ pubsub.js
@@ -1,24 +1,48 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST:'TEST'
+    TEST:'TEST',
+    BLOCKCHAIN:'BLOCKCHAIN'
 };
 
 
 class PubSub{
-    constructor(){
+    constructor({blockchain}){
+        this.blockchain=blockchain;
         this.pusblisher = redis.createClient();
         this.subscriber = redis.createClient();
-        this.subscriber.subscribe(CHANNELS.TEST);
-        this.subscriber.on('message',(channel,message)=>this.handleMessage(channel,message))
+        this.subscribeToChannels();
+        this.subscriber.on('message',(channel,message)=>this.handleMessage(channel,message));
+        
     }
     handleMessage(channel,message){
         console.log(`Message received. Channel ${channel}. Message:${message}.`);
+        const parsedMessage = JSON.parse(message);
+        if(channel == CHANNELS.BLOCKCHAIN){
+            this.blockchain.replaceChain(parsedMessage);
+        }
+    }
+    subscribeToChannels(){
+        Object.values(CHANNELS).forEach((channel)=>{
+            this.subscriber.subscribe(channel);
+        })
+    }
+    publish({channel,message}){
+        this.pusblisher.publish(channel,message);
+    }
+    broadcastChain(){
+        this.publish({
+            channel:CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
     }
 }
 
-const testPubSub = new PubSub();
-setTimeout(()=>testPubSub.pusblisher.publish(CHANNELS.TEST,'foo'),1000);
+module.exports = PubSub;
+
+
+
+
 
 
 
